4f8317fb7e66a888a1bc0a4935bbe4e21cbbbbe7
MohitRelaha <43386001+MohitRelaha@users.noreply.github.com>
2020-01-11 21:03:17 +0530

    Update main.js

--- Front-End/src/components/main.js
+++ Front-End/src/components/main.js
@@ -8,6 +8,8 @@ import PageNotFound from './error404'
 import Forgotpw from './forgotpw'
 import AddSongs from './addsongs'
 import AllSongs from './allsongs'
+import AddArtist from './addartist'
+import Browse from './browse'
 import './stylelogin.css'
 import './universal.css'
 
@@ -18,18 +20,26 @@ class Main extends Component {
     constructor(){
         super()
         this.state = {
-            arr:[]
+            arr:[],
+            artistArr:[]
         }
     }
 
 
     componentDidMount(){
-        fetch('http://localhost:3005/metadata')
+        fetch('http://localhost:3005/tracks/metadata')
             .then(res => res.json())   // return json format : actual data is always in form of string
             .then(res => {
               this.setState({arr : res})
               console.log('arr = ' + this.state.arr)
             })
+
+        fetch('http://localhost:3005/tracks/artist')
+        .then(res => res.json())   // return json format : actual data is always in form of string
+        .then(res => {
+            this.setState({artistArr : res})
+            console.log('ArtistArr = ' + this.state.artistArr)
+        })
     }
 
 
@@ -40,10 +50,12 @@ class Main extends Component {
                     <Route exact path ='/' render={() => (<div id="background"><Home/></div>)}/>
                     <Route exact path ='/login' render={() => (<div id="background"><Login/> <Title/></div>)}/> 
                     <Route exact path ='/signup' render={() => (<div id="background"><Signup/> <Title/></div>)}/>
-                    <Route exact path ='/dashboard' render={() => (<DashBoard arr={this.state.arr}/>)}/>
+                    <Route exact path ='/dashboard' render={() => (<DashBoard arr={this.state.arr} artistArr={this.state.artistArr}/>)}/>
                     <Route exact path ='/forgotpw' render={() => (<Forgotpw/>)}/>
                     <Route exact path ='/dashboard/addSongs' render={() => (<AddSongs/>)} />
+                    <Route exact path ='/dashboard/addArtist' render={() => (<AddArtist/>)} />
                     <Route exact path ='/dashboard/allSongs' render={() => (<AllSongs arr={this.state.arr}/>)}/>
+                    {/* <Route exact path ='/browse' render={() => (<Browse artistArr={this.state.artistArr} arr={this.state.arr}/>)}/> */}
                     <Route render={() => (<PageNotFound/>)}/>
                 </Switch>
             </div>

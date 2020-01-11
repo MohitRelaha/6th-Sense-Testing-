dab22ee4fb485d03a8cf8d6e0fab82fe8ce88114
MohitRelaha <43386001+MohitRelaha@users.noreply.github.com>
2020-01-11 20:58:32 +0530

    Update browse.js

--- Front-End/src/components/browse.js
+++ Front-End/src/components/browse.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import './styledash.css'
+import NavBar from './navbar.js'
+import Display from './display.js'
 
 export default class browse extends Component {
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            currentURL:"browse",
+            artist:"",
+            playingBar:false
+        }
+    }
+    
+    
+    
     render() {
         return (
-            <div>
-                <h1 style={{color:"black"}}>BROWSE</h1>
+            <div id="mainContainer"> 
+                <div id="topContainer">
+                    <NavBar/>
+                    <Display arr={this.props.arr} artistArr={this.props.artistArr} currentURL={this.state.currentURL}/>
+                </div>
+                
             </div>
         )
     }

e647ebff8a29572ac28ecdc09af5f8dca54fe469
MohitRelaha <43386001+MohitRelaha@users.noreply.github.com>
2020-01-22 21:13:37 +0530

    Update display.js

--- Front-End/src/components/display.js
+++ Front-End/src/components/display.js
@@ -4,20 +4,43 @@ import './styledash.css'
 import AllSongs from './allsongs'
 import Browse from './browse'
 import AllArtists from './allArtists'
+import Language from './language'
+import Search from './search'
 
 export class Display extends Component {
 
     render() {
+        if(this.props.currentURL === 'allSongs'){
         return (
             
                 <div id="displayPartContainer">
                     <div id="displayPart">
-                        {this.props.currentURL === 'allSongs' && <AllSongs arr={this.props.arr} play={this.props.play} pause={this.props.pause} stop={this.props.stop} artistSelected={this.props.artistSelected}/>}
-                        {this.props.currentURL === 'browse' && <AllArtists arr={this.props.arr} artistArr={this.props.artistArr} artistBrowsed={this.props.artistBrowsed} />}    
+                        <AllSongs arr={this.props.arr} play={this.props.play} pause={this.props.pause} stop={this.props.stop} artistSelected={this.props.artistSelected} langSelected={this.props.langSelected}/>
                     </div>
                 </div>
-             
+
         )
+        }
+        else if(this.props.currentURL === 'browse'){
+            return(
+                <div id="browseContainer">
+                    <div id="displayPart">
+                        <AllArtists arr={this.props.arr} artistArr={this.props.artistArr} stopIt={this.props.stopIt}/>
+                        <Language arr={this.props.arr} artistArr={this.props.artistArr} />
+                    </div>
+                </div>
+            )
+        }
+        else if(this.props.currentURL === 'search'){
+            return(
+                <div id="browseContainer">
+                    <div id="displayPart">
+                    <Search arr={this.props.arr} artistArr={this.props.artistArr} play={this.props.play} pause={this.props.pause} stop={this.props.stop} artistSelected={this.props.artistSelected} langSelected={this.props.langSelected} stopIt={this.props.stopIt}/>
+                    
+                    </div>
+                </div>
+            )
+        }
     }
 }
 

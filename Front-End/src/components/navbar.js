9997e1e4c9302a4207be93769e9172061f2da9b5
MohitRelaha <43386001+MohitRelaha@users.noreply.github.com>
2020-01-22 21:18:47 +0530

    Update navbar.js

--- Front-End/src/components/navbar.js
+++ Front-End/src/components/navbar.js
@@ -3,20 +3,33 @@ import {Link} from 'react-router-dom'
 import './styledash.css'
 import logo from '../images/logo.png'
 import search from '../images/search.png'
+import Cookies from 'universal-cookie';
 
-export class Navbar extends Component {
+const cookies = new Cookies();
+
+class Navbar extends Component {
+    
+    constructor(){
+        super()
+        this.state = {
+            username: cookies.get('username') 
+        }
+    }
+    
+    
     render() {
+        //console.log("User Name : " + this.state.username)
         return (
-            
+                
                 <div id="navBarContainer">
                     <nav className="navBar">
                         
-                        <Link to="/"><img src={logo} alt="logo"/></Link>
+                        <Link to="/" onClick={()=>this.props.stopIt()} ><img src={logo} alt="logo"/></Link>
                         
                         <div className="group">
                             
                             <div className="navItem">
-                                <Link to="search" className="navItemLink">Search
+                                <Link to="/dashboard/search" className="navItemLink" onClick={()=>this.props.stopIt()} >Search
                                     <img src={search} alt="search" className="icon"/>
                                 </Link>
                             </div>
@@ -27,23 +40,24 @@ export class Navbar extends Component {
                         <div className="group">
                             
                             <div className="navItem">
-                                <span className="navItemLink" onClick={()=>this.props.changeURL()}>Browse</span>
+                            {/* <span className="navItemLink" onClick={()=>this.props.changeURL()}>Browse</span> */}
+                                <Link to="/dashboard/browse"><span onClick={()=>this.props.stopIt()} className="navItemLink">Browse</span></Link>
                             </div>
                             
                             <div className="navItem">
-                                <Link to="yourMusic" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Your Music</span></Link>
+                                <Link to="/yourMusic" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Your Music</span></Link>
                             </div>
                             
                             <div className="navItem">
-                                <Link to="profile" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Mohit Relaha</span></Link>
+                                <Link to="/profile" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Profile</span></Link>
                             </div>
-                            
+                        
                             <div className="navItem">
-                                <Link to="dashboard/addSongs" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Add Songs</span></Link>
+                                <Link to="/dashboard/addSongs" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Add Songs</span></Link>
                             </div>
 
                             <div className="navItem">
-                                <Link to="dashboard/addArtist" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Add Artist</span></Link>
+                                <Link to="/dashboard/addArtist" ><span onClick={()=>this.props.stopIt()} className="navItemLink">Add Artist</span></Link>
                             </div>
                         </div>
                         
